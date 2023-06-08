521cbf5de8d0a293826c1d7eb3cdde399c570cd8
bianchimarco <m.bianchi@aruba.it>
2023-06-08 12:19:55 +0200

    End first part without sending form data
    the form data was not sent, just a console log message. It will be added later.

---
+++ booking-ristorante.js
@@ -0,0 +1,90 @@
+// create an empty object
+const Booking = {};
+// retrieve the numero-persone-w wrapper
+Booking.numeroPersoneW = document.getElementById('numero-persone-w');
+// retrieve the "persone" span element
+Booking.numeroPersone = document.getElementById('numero-persone');
+// comment the tavoli div wrapper to recreate the tables with js and retrieve the tavoli-w wrapper
+Booking.tavoliW = document.getElementById('tavoli-w');
+// retrieve the 'tavolo-selezionato' id
+Booking.tavoloSelezionato = document.getElementById('tavolo-selezionato');
+// retrieve the message status id
+Booking.messageStatus = document.getElementById('message-status');
+// create a file sala.json to simulate the tables booking
+
+(async function costruisciSala() {
+    Booking.sala = await fetch('sala.json');
+    Booking.sala = await Booking.sala.json();
+   // console.log(Booking.sala);
+    // console.log(Booking.sala.tavoli);
+    Booking.tavoli = Booking.sala.tavoli;
+    disponiTavoli(Booking.tavoli);
+})();
+
+// create function disponiTavoli
+function disponiTavoli(tavoli) {
+    tavoli.forEach((tavolo, i) => {
+        let classiTavolo = 'tavolo';
+        let tavoloDOM = document.createElement('div');
+        tavoloDOM.appendChild(document.createTextNode(i + 1));
+        classiTavolo += tavolo.occupato ? ' occupato' : ' libero';
+       // si può scrivere anche così:  classiTavolo = classiTavolo + (tavolo.occupato ? ' occupato' : ' libero');
+        classiTavolo += tavolo.posti == 6 ? ' x6' : ' x4';
+        tavoloDOM.setAttribute('class', classiTavolo);
+        Booking.tavoliW.appendChild(tavoloDOM);
+    });
+    
+}
+
+// manage the persons number
+Booking.numeroPersoneW.addEventListener('click', (e) => {
+    e.preventDefault();
+    // retrieve numero persone
+    let numeroPersone = +Booking.numeroPersone.textContent;
+    // retrieve the user action (add or sub)
+    if (e.target.id === 'add') {
+        Booking.numeroPersone.textContent = numeroPersone + 1;
+    } else if (e.target.id === 'sub' && numeroPersone > 1) {
+        Booking.numeroPersone.textContent = numeroPersone - 1;
+    }
+});
+
+// manage the tables booking
+Booking.tavoliW.addEventListener('click', (e) => {
+   let selezionato = e.target.textContent;
+  // console.log(selezionato);
+    if (Booking.tavoli[selezionato - 1].occupato) {
+        Booking.messageStatus.textContent = `Il tavolo ${selezionato} è occupato`;
+    } else {
+        Booking.tavoloSelezionato.textContent = selezionato;
+    }
+});
+
+// sending booking
+document.forms[0].addEventListener('submit', (e) => {
+   e.preventDefault();
+   // checking if table is selected
+    if (Booking.tavoloSelezionato.textContent == '-') {
+        Booking.messageStatus.textContent = "è necessario selezionare un tavolo";
+        return;
+    }
+
+    sendBooking();
+});
+
+function sendBooking() {
+    let bookingForm = new FormData;
+    // retrieve persons number and selected table
+    bookingForm.append('numero-persone', +Booking.numeroPersone.textContent);
+    bookingForm.append('tavolo-selezionato', +Booking.tavoloSelezionato.textContent);
+    // retrieve form data
+    bookingForm.append('nome', document.forms[0].nome.value);
+    bookingForm.append('email', document.forms[0].email.value);
+   // console.log('prenotazione inviata');
+    /* fetch('bookingScript', {
+        body: bookingForm,
+        method: 'post'
+    }); */
+    Booking.messageStatus.textContent = "la prenotazione è andata a buon fine";
+    document.forms[0].reset();
+}
